dbdc4577b51248eb35d4f91f71cf8aea5fec3b58
kaVII-x <ka52jiguli@yandex.ru>
2024-04-01 19:05:59 +0400

    first_app_test

---
+++ src/main/java/com/yanfeofanov/test_app_wallet/TestAppWalletApplication.java
@@ -0,0 +1,13 @@
+package com.yanfeofanov.test_app_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAppWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yanfeofanov/test_app_wallet/TestAppWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanfeofanov.test_app_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
